7c5a25ab46934a851b4a76f9f0636791788e24d4
Nick Ponvert <nickponvert@gmail.com>
2020-07-04 14:04:32 -0700

    Correct license in setup.py

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        "License :: Apache-2.0",
         "Operating System :: OS Independent",
     ],
     python_requires='==2.7.18',
